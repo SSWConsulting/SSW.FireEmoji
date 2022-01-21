fcba37e5219dcc64df503653b32c8bbfa5fc7c84
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2022-01-21 16:00:12 +1000

    ✨ Added ML for gitmojis.

---
+++ src/SSW.FireEmoji/SSW.FireMoji.Trainer/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.ML;
+using SSW.FireEmoji.Core.MachineLearning;
+using SSW.FireEmoji.Core.Models;
+
+Console.WriteLine("Hello, World!");
+
+string modelName = "gitmo.mlnet";
+
+bool forceRebuild = false;
+ITransformer? model = null;
+if (!File.Exists(modelName) || forceRebuild)
+{
+    GitmoTrainer trainer = new();
+    var data = trainer.MlContext.Data.LoadFromTextFile<GitComment>(@"Data\CommitMessages.csv", separatorChar: ',', hasHeader: false);
+
+    model = trainer.AutoTrain(data, 90);
+    trainer.SaveModel(modelName, model);
+}
+
+GitmoPredictor gitmoPredictor = new();
+if (model != null)
+{
+    gitmoPredictor.SetModel(model);
+}
+else
+{
+    gitmoPredictor.LoadFromFilePath(modelName);
+}
+
+Predict("Init project");
+
+void Predict(string commit)
+{
+    var prediction = gitmoPredictor.Predict(new GitComment {  Comment = commit });
+    Console.WriteLine("Prediction for " + commit);
+    Console.WriteLine(prediction.Emoji + " " + commit);
+}
