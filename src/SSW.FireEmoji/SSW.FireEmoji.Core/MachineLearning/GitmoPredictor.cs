e8833a884f9cebfd3cf3980e9a6af01f79c3c2f3
Jernej Kavka (JK) [SSW] <jernejk@users.noreply.github.com>
2022-01-21 16:03:15 +1000

    ✨ Added ML for gitmojis. (#11)

---
+++ src/SSW.FireEmoji/SSW.FireEmoji.Core/MachineLearning/GitmoPredictor.cs
@@ -0,0 +1,15 @@
+﻿using SSW.FireEmoji.Core.Models;
+
+namespace SSW.FireEmoji.Core.MachineLearning;
+
+public interface IGitmoPredictor
+{
+    List<string> GetCategories();
+    GitmojiPrediction Predict(GitComment input);
+    Dictionary<string, float> PredictionList(GitComment input);
+}
+
+public class GitmoPredictor : BasePredictor<GitComment, GitmojiPrediction>, IGitmoPredictor
+{
+    // NOTE: No implementation required.
+}
---
+++ src/SSW.FireEmoji/SSW.FireEmoji.Core/Models/GitComment.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Data;
+
+namespace SSW.FireEmoji.Core.Models;
+
+public class GitComment
+{
+    [LoadColumn(0)]
+    [ColumnName("col0")]
+    public string? Emoji { get; set; }
+
+    [LoadColumn(1)]
+    [ColumnName("col1")]
+    public string? Comment { get; set; }
+}
