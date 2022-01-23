6d5c6a9bae80e61b3af5693b223f61256e8446aa
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2022-01-24 09:05:31 +1000

    ✨ Add copy button (#14)
    * 📝  Update readme
    
    * 🌱  Fix invalid data
    
    * ✨ Add copy button
    
    * 📝 Update readme
    
    * 💚  Fix build action
    
    * 🔧 Add rider files

--- src/SSW.FireEmoji/SSW.FireEmoji.WebApp/Program.cs
+++ src/SSW.FireEmoji/SSW.FireEmoji.WebApp/Program.cs
@@ -1,3 +1,4 @@
+using Append.Blazor.Clipboard;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SSW.FireEmoji.WebApp;
@@ -6,7 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddAntDesign();
+builder.Services.AddClipboard();
 
 await builder.Build().RunAsync();
