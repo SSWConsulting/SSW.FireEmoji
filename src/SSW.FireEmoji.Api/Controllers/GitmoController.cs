6b20c9c7d49cc2d265b7f649f0424ffe3e698f99
Jack Pettit [SSW] <57518417+JackDevAU@users.noreply.github.com>
2022-02-03 11:48:09 +1000

    🚧 Api + Console Application (#22)

---
+++ src/SSW.FireEmoji.Api/Controllers/GitmoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SSW.FireEmoji.Core.MachineLearning;
+using SSW.FireEmoji.Core.Models;
+
+namespace SSW.FireEmoji.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GitmoController : ControllerBase
+    {
+        GitmoPredictor gitmoPredictor = new GitmoPredictor();
+        private static string ModelStreamUri => "gitmo.mlnet";
+
+        public GitmoController()
+        {
+            LoadModel();
+        }
+
+        private void LoadModel()
+        {
+            gitmoPredictor.LoadFromFilePath(ModelStreamUri);
+        }
+
+        [HttpGet()]
+        public GitmojiPrediction Get(string commit)
+        {
+            return gitmoPredictor.Predict(new GitComment() { Comment = commit });
+        }
+    }
+}
