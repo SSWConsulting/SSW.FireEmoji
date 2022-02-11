cd1bcd4f268b2eaa1de11379d13e7e5ee5fee9d0
Jernej Kavka (JK) <jk@ssw.com.au>
2022-02-11 23:49:50 +1000

    ♻️ Refactor trainer to improve predction console size

---
+++ src/SSW.FireEmoji.Trainer.Core/GitmoTrainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using SSW.FireEmoji.Core.Models;
+
+namespace SSW.FireEmoji.Trainer.Core;
+
+public class GitmoTrainer : BaseTrainer<GitComment>
+{
+    public GitmoTrainer(MLContext? mlContext = null)
+        : base(mlContext) { }
+
+    protected override ColumnInformation GetColumnInformation()
+    {
+        ColumnInformation columnInfo = new() { LabelColumnName = "col0" };
+        columnInfo.TextColumnNames.Add("col1");
+
+        return columnInfo;
+    }
+}
